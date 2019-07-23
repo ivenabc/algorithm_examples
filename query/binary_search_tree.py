3550bf04c40db0d93c9995ac2c0dedb703bf66ac
ivenabc <hopeicc@163.com>
2019-07-23 18:39:18 +0800

    binary search tree

--- query/binary_search_tree.py
+++ query/binary_search_tree.py
@@ -12,7 +12,7 @@ class Node:
 
 class BinarySearchTree:
     def __init__(self):
-        self.root = Node('default', 1)
+        self.root = None
     
 
     @staticmethod
@@ -46,6 +46,56 @@ class BinarySearchTree:
         node.n = BinarySearchTree.size_node(node.left) + BinarySearchTree.size_node(node.right) + 1
         return node
 
+    @staticmethod
+    def min_node(node):
+        if not node.left: return node
+        return BinarySearchTree.min_node(node.left)
+    
+    @staticmethod
+    def select_node(node, k):
+        if not node: return None
+        t = BinarySearchTree.size_node(node.left)
+        if t > k:
+            return BinarySearchTree.select_node(node.left, k)
+        elif t < k:
+            return BinarySearchTree.select_node(node.right, k-t-1)
+        else:
+            return node 
+    
+    @staticmethod
+    def delete_min_node(node):
+        if not node.left: return node.right
+        x.left = BinarySearchTree.delete_min_node(ndoe.left) 
+    
+    @staticmethod
+    def delete_node(node, key):
+        if not node: return None
+        if key > node.key:
+            node.right = BinarySearchTree.delete_node(node.right, key)
+        elif key < node.key:
+            node.left = BinarySearchTree.delete_node(node.left, key)
+        else:
+            if not node.right: return node.left 
+            if not node.left: return node.right
+            t = node 
+            node = min(t.right)
+            node.right = BinarySearchTree.delete_min_node(node.right)
+            node.left = t.left
+    
+    def delete(self,key):
+        self.root = BinarySearchTree.delete_node(self.root, key)
+
+
+    def delete_min():
+        self.root = BinarySearchTree.delete_min_node(self.root)
+
+    def select(self, k):
+        return BinarySearchTree.select_node(self.root, k)
+    
+    def min(self):
+        return BinarySearchTree.min_node(self.root)
+
+
     def put(self, key, value):
         self.root = BinarySearchTree.put_node(self.root, key, value)
 
@@ -55,8 +105,13 @@ class BinarySearchTree:
 
 if __name__ == '__main__':
     bst = BinarySearchTree()
+    bst.put('h', 0)
     bst.put('k', 2)
-    bst.put('a', 4)
+    bst.put('c', 4)
     bst.put('d', 5)
 
-    print(bst.get('a').n)
+    print(bst.get('d').n)
+    print(bst.select(1).key)
+    print(bst.min().key)
+    bst.delete('k')
+    print(bst.get('k'))
